b091d3b841ba245681e26cb2a68e642dbe0a3e5b
kallesedergren <kalle.sedergren@gmail.com>
2016-02-03 12:39:15 +0200

    ummmm

---
+++ Teht1/Elevator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Elevator
+    {
+        private readonly int FloorValue = 1;
+        private int floorvalue;
+
+        public int Floor
+        {
+            get
+            {
+                return floorvalue;
+            }
+            set
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("There's no such floor, go up. ");
+                    floorvalue = FloorValue;
+                }
+                else if (value > 5)
+                {
+                    Console.WriteLine("There's no such floor, go down. ");
+                    floorvalue = FloorValue;
+                }
+                else
+                {
+                    floorvalue = value;
+                }
+            }
+
+        }
+    }
+}
---
+++ Teht1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+            elevator.Floor = 1;
+            int valinta = 1;
+
+            while (valinta != 0)
+            {
+                Console.WriteLine("Welcome to the elevator! Please select a floor you want to go: ");
+                valinta = int.Parse(Console.ReadLine());
+                switch (valinta)
+                {
+                    case 1:
+                        elevator.Floor = 1;
+                        Console.WriteLine("Current floor: " + elevator.Floor);
+                        break;
+                    case 2:
+                        elevator.Floor = 2;
+                        Console.WriteLine("Current floor: " + elevator.Floor);
+                        break;
+                    case 3:
+                        elevator.Floor = 3;
+                        Console.WriteLine("Current floor: " + elevator.Floor);
+                        break;
+                    case 4:
+                        elevator.Floor = 4;
+                        Console.WriteLine("Current floor: " + elevator.Floor);
+                        break;
+                    case 5:
+                        elevator.Floor = 5;
+                        Console.WriteLine("Current floor: " + elevator.Floor);
+                        break;
+                }
+            } 
+            
+            
+              
+        }
+    }
+}
